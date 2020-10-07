1dcf5ccbfabea0d66b4b2b9f8df4f2ccfd744449
MuhammadShafiqAzuka <45938624+MuhammadShafiqAzuka@users.noreply.github.com>
2020-10-08 00:37:36 +0800

    Get from url and QUERy based on currency

--- app/src/main/java/com/azuka/currencyapp/component/RetrofitInterface.java
+++ app/src/main/java/com/azuka/currencyapp/component/RetrofitInterface.java
@@ -4,9 +4,9 @@ import com.google.gson.JsonObject;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface RetrofitInterface {
-    @GET("/latest?base={currency}")
-    Call<JsonObject> getExchangeCurrency(@Path("currency") String currency);
+    @GET("latest?base=EUR HTTP/1.1")
+    Call<JsonObject> getExchangeCurrency(@Query("currency") String currency);
 }
