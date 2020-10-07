d60c2f2c8bce5f6783c81ec384082d7052216dde
MuhammadShafiqAzuka <45938624+MuhammadShafiqAzuka@users.noreply.github.com>
2020-10-07 20:02:56 +0800

    Derive the result from component module

--- app/src/main/java/com/azuka/currencyapp/view/MainActivity.java
+++ app/src/main/java/com/azuka/currencyapp/view/MainActivity.java
@@ -1,34 +1,75 @@
 package com.azuka.currencyapp.view;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.azuka.currencyapp.R;
+import com.azuka.currencyapp.component.RetrofitBuilder;
+import com.azuka.currencyapp.component.RetrofitInterface;
+import com.google.gson.JsonObject;
 
-import java.text.BreakIterator;
-import java.util.List;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static BreakIterator data;
-    List<String> keysList;
-    Spinner toCurrency;
-    TextView textView;
+    Button button;
+    EditText currencyToBeConverted;
+    EditText currencyConverted;
+    Spinner convertToDropdown;
+    Spinner convertFromDropdown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toCurrency = findViewById(R.id.planets_spinner);
-        final EditText edtEuroValue = findViewById(R.id.editText4);
-        final Button btnConvert = findViewById(R.id.button);
-        textView = findViewById(R.id.textView7);
+        //Initialization
+        currencyConverted = findViewById(R.id.currency_converted);
+        currencyToBeConverted = findViewById(R.id.currency_to_be_converted);
+        convertToDropdown = findViewById(R.id.convert_to);
+        convertFromDropdown = findViewById(R.id.convert_from);
+        button = findViewById(R.id.button);
 
+        //Adding Functionality
+        String[] dropDownList = {"USD", "INR", "EUR", "NZD"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, dropDownList);
+        convertToDropdown.setAdapter(adapter);
+        convertFromDropdown.setAdapter(adapter);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RetrofitInterface retrofitInterface = RetrofitBuilder.getRetrofitInstance().create(RetrofitInterface.class);
+                Call<JsonObject> call = retrofitInterface.getExchangeCurrency(convertFromDropdown.getSelectedItem().toString());
+                call.enqueue(new Callback<JsonObject>() {
+                    @Override
+                    public void onResponse(@NotNull Call<JsonObject> call, @NotNull Response<JsonObject> response) {
+                        JsonObject res = response.body();
+                        JsonObject rates = Objects.requireNonNull(res).getAsJsonObject("rates");
+                        double currency = Double.parseDouble(currencyToBeConverted.getText().toString());
+                        double multiplier = Double.parseDouble(rates.get(convertToDropdown.getSelectedItem().toString()).toString());
+                        double result = currency * multiplier;
+                        currencyConverted.setText(String.valueOf(result));
+                    }
+
+                    @Override
+                    public void onFailure(@NotNull Call<JsonObject> call, @NotNull Throwable t) {
+
+                    }
+                });
+            }
+        });
     }
 }
