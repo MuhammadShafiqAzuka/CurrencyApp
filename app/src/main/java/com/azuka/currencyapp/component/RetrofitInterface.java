9d5d520514f1963fe3ef1b35614a31e7deda7e7b
MuhammadShafiqAzuka <45938624+MuhammadShafiqAzuka@users.noreply.github.com>
2020-10-08 22:22:55 +0800

    Retrofit Initialize, Builder, Calling

--- app/src/main/java/com/azuka/currencyapp/component/RetrofitInterface.java
+++ app/src/main/java/com/azuka/currencyapp/component/RetrofitInterface.java
@@ -4,9 +4,9 @@ import com.google.gson.JsonObject;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Query;
+import retrofit2.http.Path;
 
 public interface RetrofitInterface {
     @GET("v4/latest/{currency}")
-    Call<JsonObject> getExchangeCurrency(@Query("currency") String currency);
+    Call<JsonObject> getExchangeCurrency(@Path("currency") String currency);
 }
