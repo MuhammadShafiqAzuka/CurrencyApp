37c4f841e4ff991d71819a21524fc71d137fe3d2
MuhammadShafiqAzuka <45938624+MuhammadShafiqAzuka@users.noreply.github.com>
2020-10-08 22:23:29 +0800

    Firebase Database Initialization

--- app/src/main/java/com/azuka/currencyapp/view/MainActivity.java
+++ app/src/main/java/com/azuka/currencyapp/view/MainActivity.java
@@ -5,15 +5,21 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.azuka.currencyapp.R;
 import com.azuka.currencyapp.component.RetrofitBuilder;
 import com.azuka.currencyapp.component.RetrofitInterface;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.gson.JsonObject;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.text.DecimalFormat;
+import java.util.HashMap;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -27,10 +33,21 @@ public class MainActivity extends AppCompatActivity {
     Spinner convertToDropdown;
     Spinner convertFromDropdown;
 
+    //firebase
+    DatabaseReference RootRef;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        InitializeVariables();
+        CurrencyChange();
+
+    }
+
+    private void InitializeVariables() {
 
         //Initialization
         currencyConverted = findViewById(R.id.currency_converted);
@@ -45,6 +62,12 @@ public class MainActivity extends AppCompatActivity {
         convertToDropdown.setAdapter(adapter);
         convertFromDropdown.setAdapter(adapter);
 
+        //Firebase Fucntion
+        //create user to db
+        RootRef = FirebaseDatabase.getInstance().getReference().child("Currency");
+    }
+
+    public void CurrencyChange() {
         button.setOnClickListener(v -> {
             RetrofitInterface retrofitInterface = RetrofitBuilder.getRetrofitInstance().create(RetrofitInterface.class);
             Call<JsonObject> call = retrofitInterface.getExchangeCurrency(convertFromDropdown.getSelectedItem().toString());
@@ -57,7 +80,9 @@ public class MainActivity extends AppCompatActivity {
                     double currency = Double.parseDouble(currencyToBeConverted.getText().toString());
                     double multiplier = Double.parseDouble(rates.get(convertToDropdown.getSelectedItem().toString()).toString());
                     double result = currency * multiplier;
-                    currencyConverted.setText(String.valueOf(result));
+                    DecimalFormat precision = new DecimalFormat("0.00");
+                    currencyConverted.setText(precision.format(result));
+                    SaveToDatabase(currency, result);
                 }
 
                 @Override
@@ -65,7 +90,24 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         });
+    }
 
+    private void SaveToDatabase(double currency, double result) {
+
+        DecimalFormat precision = new DecimalFormat("0.00");
+        String from = convertFromDropdown.getSelectedItem().toString();
+        String to = convertToDropdown.getSelectedItem().toString();
+
+        HashMap<String, Object> CurrencyMap = new HashMap<>();
+        CurrencyMap.put("from", from);
+        CurrencyMap.put("results", precision.format(result));
+        CurrencyMap.put("to", to);
+        CurrencyMap.put("values", precision.format(currency));
+
+        RootRef.child("Results").
+                updateChildren(CurrencyMap)
+                .addOnCompleteListener(task -> Toast.makeText(MainActivity.this, "Value Added", Toast.LENGTH_SHORT).show())
+                .addOnFailureListener(e -> Toast.makeText(MainActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show());
 
     }
 }
