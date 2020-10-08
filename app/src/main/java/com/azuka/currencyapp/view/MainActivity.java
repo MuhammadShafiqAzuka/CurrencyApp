e94ae8166ef85e30e3a077cfae3e15a47c791a11
MuhammadShafiqAzuka <45938624+MuhammadShafiqAzuka@users.noreply.github.com>
2020-10-08 21:11:20 +0800

    Retrofit Initialize, Builder, Calling

--- app/src/main/java/com/azuka/currencyapp/view/MainActivity.java
+++ app/src/main/java/com/azuka/currencyapp/view/MainActivity.java
@@ -1,7 +1,6 @@
 package com.azuka.currencyapp.view;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,8 +14,6 @@ import com.azuka.currencyapp.component.RetrofitInterface;
 import com.google.gson.JsonObject;
 
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Objects;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -43,33 +40,32 @@ public class MainActivity extends AppCompatActivity {
         button = findViewById(R.id.button);
 
         //Adding Functionality
-        String[] dropDownList = {"USD", "INR", "EUR", "NZD"};
+        String[] dropDownList = {"EUR", "INR", "USD", "NZD"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, dropDownList);
         convertToDropdown.setAdapter(adapter);
         convertFromDropdown.setAdapter(adapter);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                RetrofitInterface retrofitInterface = RetrofitBuilder.getRetrofitInstance().create(RetrofitInterface.class);
-                Call<JsonObject> call = retrofitInterface.getExchangeCurrency(convertFromDropdown.getSelectedItem().toString());
-                call.enqueue(new Callback<JsonObject>() {
-                    @Override
-                    public void onResponse(@NotNull Call<JsonObject> call, @NotNull Response<JsonObject> response) {
-                        JsonObject res = response.body();
-                        JsonObject rates = Objects.requireNonNull(res).getAsJsonObject("rates");
-                        double currency = Double.parseDouble(currencyToBeConverted.getText().toString());
-                        double multiplier = Double.parseDouble(rates.get(convertToDropdown.getSelectedItem().toString()).toString());
-                        double result = currency * multiplier;
-                        currencyConverted.setText(String.valueOf(result));
-                    }
+        button.setOnClickListener(v -> {
+            RetrofitInterface retrofitInterface = RetrofitBuilder.getRetrofitInstance().create(RetrofitInterface.class);
+            Call<JsonObject> call = retrofitInterface.getExchangeCurrency(convertFromDropdown.getSelectedItem().toString());
+            call.enqueue(new Callback<JsonObject>() {
+                @Override
+                public void onResponse(@NotNull Call<JsonObject> call, @NotNull Response<JsonObject> response) {
+                    JsonObject res = response.body();
+                    assert res != null;
+                    JsonObject rates = res.getAsJsonObject("rates");
+                    double currency = Double.parseDouble(currencyToBeConverted.getText().toString());
+                    double multiplier = Double.parseDouble(rates.get(convertToDropdown.getSelectedItem().toString()).toString());
+                    double result = currency * multiplier;
+                    currencyConverted.setText(String.valueOf(result));
+                }
 
-                    @Override
-                    public void onFailure(@NotNull Call<JsonObject> call, @NotNull Throwable t) {
-
-                    }
-                });
-            }
+                @Override
+                public void onFailure(@NotNull Call<JsonObject> call, @NotNull Throwable t) {
+                }
+            });
         });
+
+
     }
 }
